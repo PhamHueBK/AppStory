ef50c22392374f226a365bd53584338864757fcf
PhamHue <Phamhue97@gmail.com>
2018-03-25 09:37:58 +0700

    Hue_UPload

---
+++ Project/Project/Model/FormParagraphModel/ParagraphModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model.FormParagraphModel
+{
+    public class ParagraphModel
+    {
+        public long paragraphId { get; set; }
+        public long pathId { get; set; }
+        public string content { get; set; }
+    }
+}
---
+++ Project/Project/Model/FormPathModel/PathModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model.FormPathModel
+{
+    public class PathModel
+    {
+        public long pathId { get; set; }
+
+        public long postId { get; set; }
+
+        public string title { get; set; }
+    }
+}
