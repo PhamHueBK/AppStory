176df3700a7bdf2c4947dc75d6dcca4f0d529d08
do-khac-hung <khachungbg97@gmail.com>
2018-04-04 08:00:39 +0700

    Upload

--- Project/Project/Connection/connect.cs
+++ Project/Project/Connection/connect.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data;
 
 namespace Project.Connection
 {
     public class connect
     {
-        public string connecting = "datasource=localhost;database=storyapp;username=root;password=";
-        public MySqlConnection connection = new MySqlConnection(connecting);
-        public void query(sqlQuery)
+        string connecting = "datasource=localhost;database=storyapp1;username=root;password=";
+        MySqlConnection connection = new MySqlConnection();
+        MySqlConnection sql = new MySqlConnection("datasource=localhost;database=storyapp1;username=root;password=");
+        public void query(string sqlQuery)
         {
             MySqlCommand cmd = new MySqlCommand(sqlQuery, this.connection);
             this.connection.Open();
--- Project/Project/ViewModel/FormPost/PostViewModel.cs
+++ Project/Project/ViewModel/FormPost/PostViewModel.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using Project.Model;
 using MySql.Data.MySqlClient;
-using Project.Connection;
-using Project.Model.FormPost;
 
 namespace Project.ViewModel.FormPost
 {
     public class PostViewModel
     {
-        public MySqlConnection connection = new connect();
-        public MySqlConnection connect = connect.query("SELECT * FROM posts");
-        public PostModel[] posts;
-        public void loadData()
+        
+        public DataTable loadData()
         {
+            string connecting = "datasource=localhost;database=storyapp;username=root;password=";
+            MySqlConnection connection = new MySqlConnection(connecting);
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM `posts`", connection);
+            connection.Open();
             DataTable data = new DataTable();
             data.Load(cmd.ExecuteReader());
-            this.connect.Close();
-            posts = data;
+            connection.Close();
+            Console.Write(data);
+            return data;
         }
     }
 }
